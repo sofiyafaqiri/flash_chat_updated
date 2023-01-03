429b2e04f45c5c55c78e04186bb3b061da859fa7
SomayehFaqiri <somayeh.faqiri@gmail.com>
2023-01-03 18:19:30 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flash_chat_starting_project/screens/chat_screen.dart';
+import 'package:flash_chat_starting_project/screens/login_screen.dart';
+import 'package:flash_chat_starting_project/screens/registration_screen.dart';
+
+import 'constants.dart';
+import 'package:flutter/material.dart';
+import '/screens/welcome_screen.dart';
+
+void main() {
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kBackgroundColor,
+      ),
+      routes:{
+        WelcomeScreen.id : (context)=> WelcomeScreen(),
+        LoginScreen.id: (context)=> LoginScreen(),
+        RegistrationScreen.id : (context)=> RegistrationScreen(),
+        ChatScreen.id :(context)=> ChatScreen(),
+      },
+      initialRoute: WelcomeScreen.id,
+    );
+  }
+}
