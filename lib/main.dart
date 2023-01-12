c3643f4856dd96fc0696f9321629f7280ff58929
Somayeh <sofiya.faqiri80@gmail.com>
2023-01-12 05:06:54 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:flash_chat_starting_project/firebase_options.dart';
 import 'package:flash_chat_starting_project/screens/chat_screen.dart';
 import 'package:flash_chat_starting_project/screens/login_screen.dart';
 import 'package:flash_chat_starting_project/screens/registration_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'constants.dart';
 import 'package:flutter/material.dart';
 import '/screens/welcome_screen.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(FlashChat());
 }
 
